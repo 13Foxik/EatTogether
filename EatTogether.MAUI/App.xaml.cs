303f85d4e8b931d5ab7d5f86d4eb30c6026dcf60
Roman Smeshnov <Romka.smeshnov@gmail.com>
2025-08-19 13:48:54 +0500

    Добавьте файлы проекта.

---
+++ EatTogether.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EatTogether.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EatTogether.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EatTogether.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
