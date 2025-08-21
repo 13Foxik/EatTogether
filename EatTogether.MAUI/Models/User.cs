5da6dadf90c0d57809e03999f065a512c884ff34
Roman Smeshnov <Romka.smeshnov@gmail.com>
2025-08-21 14:40:42 +0500

    class user

---
+++ EatTogether.MAUI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace EatTogether.MAUI.Models
+{
+    public class User
+    {
+        public string Uid { get; private set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; private set; }
+        public int Age { get; set; }
+        
+        public User(string uid, string displayName, string email, DateTime createdAt, int age)
+        {
+            Uid = uid;
+            DisplayName = displayName;
+            Email = email;
+            CreatedAt = createdAt;
+            Age = age;
+        }
+    }
+    //Нужен будет FirestoreDataConverter
+    //Доработать в момент создания регистрации
+}
