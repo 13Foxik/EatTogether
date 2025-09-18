e604c519484d3d7b60c4edcc423b3fb2b7353c2d
Roman Smeshnov <Romka.smeshnov@gmail.com>
2025-09-18 18:25:45 +0500

    default

--- EatTogether.MAUI/Models/User.cs
+++ EatTogether.MAUI/Models/User.cs
@@ -6,6 +6,9 @@
         public string Email { get; set; } = string.Empty;
         public string DisplayName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime DateOfBitrhDay { get; set; } = DateTime.UtcNow;
+        public bool stateOfSubscribe { get; set; } = false;
+        public string Avatar { get; set; } = string.Empty;
 
         public User() { }
 
