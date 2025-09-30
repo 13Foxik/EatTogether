c2e6fed9c814e113659e3c1234954c30be9c321f
Roman Smeshnov <Romka.smeshnov@gmail.com>
2025-09-30 14:10:37 +0500

    feat: FamilyModel v1.0

--- EatTogether.MAUI/Models/Family.cs
+++ EatTogether.MAUI/Models/Family.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Maui.Behaviors;
+using Google.Cloud.Firestore;
+using System.Data;
 
 namespace EatTogether.MAUI.Models
 {
+    [FirestoreData]
     public class Family
     {
+        [FirestoreProperty]
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string Avatar { get; set; }
+
+        [FirestoreProperty]
         public string Name { get; set; }
+
+        [FirestoreProperty]
+        public string Description { get; set; }
+
+        [FirestoreProperty]
+        public List<User?> members { get; set; }
+
+        [FirestoreProperty]
+        public int CountUsers { get; set; }
+
+        [FirestoreProperty]
+        public DateTime CreatedAt { get; set; }
+
+        public Family() { }
     }
 }
